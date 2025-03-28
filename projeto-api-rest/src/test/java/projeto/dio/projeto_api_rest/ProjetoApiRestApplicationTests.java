5cc6dd93ed9a72523204b053dcb906da8ddfbe5b
gabrielrsnts <gabrielrodriguesec63@gmail.com>
2025-03-28 15:20:46 -0300

    Initial commit

---
+++ projeto-api-rest/src/test/java/projeto/dio/projeto_api_rest/ProjetoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.dio.projeto_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
